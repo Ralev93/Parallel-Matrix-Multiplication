a324e80c6993ed2b57c7130faf0e673cb80e83ec
Hristo Ralev <hbr@abv.bg>
2015-05-31 21:57:58 +0300

    Added an 'about' button to the GUI package

---
+++ Matrix Multiplication/src/bg/uni_sofia/fmi/java/matrix_multiplication/GUI/About.java
@@ -0,0 +1,50 @@
+package bg.uni_sofia.fmi.java.matrix_multiplication.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JEditorPane;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+
+public class About extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					About frame = new About();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public About() {
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAla = new JLabel("Some text\r\n");
+		lblAla.setBounds(10, 54, 414, 128);
+		contentPane.add(lblAla);
+	}
+}
---
+++ Matrix Multiplication/src/bg/uni_sofia/fmi/java/matrix_multiplication/exceptions/IncorrectMatrixMultiplication.java
@@ -0,0 +1,20 @@
+package bg.uni_sofia.fmi.java.matrix_multiplication.exceptions;
+
+public class IncorrectMatrixMultiplication extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IncorrectMatrixMultiplication() {
+		super();
+	}
+
+	public IncorrectMatrixMultiplication(String msg) {
+		super(msg);
+	}
+
+	
+	
+}
